3fd87d2c887ebd7ef42a2ef932e073db9b0b8059
whoami <lallipapi4@yandex.ru>
2024-06-13 02:48:28 +1000

    добавил seeder для админ пользователя

---
+++ database/seeders/CreateAdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin',
+            'password' => bcrypt('CTFHKOTSO!admin2024'),
+            'status' => 'админ',
+        ]);
+    }
+}
